7282951386abda28760ce20ef21ed77f9228ba92
kenk65536 <kenny.wang@technet.com.mo>
2017-10-04 17:57:33 +0800

    site

--- sites/lunch/technet/admin.py
+++ sites/lunch/technet/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from.models import *
 admin.site.register(post)
+admin.site.register(get)
 class menuItemInline(admin.StackedInline):
   model = MenuItem
   extra = 1
--- sites/lunch/technet/models.py
+++ sites/lunch/technet/models.py
@@ -12,6 +12,17 @@ class post(models.Model):
     self.save()
   def __str__(self):
     return self.title
+class get(models.Model):
+  author = models.ForeignKey(User)
+  title = models.CharField(max_length=200)
+  text = models.TextField()
+  created_date = models.DateTimeField(default=timezone.now)
+  published_date = models.DateTimeField(blank=True, null=True)
+  def publish(self):
+    self.published_date = timezone.now()
+    self.save()
+  def __str__(self):
+    return self.title
 class store(models.Model):
   name = models.CharField(max_length=20)
   notes = models.TextField(blank=True, default='')
